bb3411483f0f208326ffc03612a92c6a338ecb21
Christian <christiancatamuscay@r-fast.com>
2023-02-21 15:26:12 -0500

    36. Lets write our first CompletableFuture(Sección 12: Exploring CompletableFuture Functions)

---
+++ parallel-asynchronous-using-java/src/main/java/com/learnjava/service/CompletableFutureHelloWorld.java
@@ -0,0 +1,22 @@
+package com.learnjava.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class CompletableFutureHelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorldService helloWorldService = new HelloWorldService();
+
+        CompletableFuture.supplyAsync(()->helloWorldService.helloWorld())
+                .thenAccept((result)->{
+                    log("Result is " + result);
+                })
+                .join();
+        log("Done!");
+        //delay(2000);
+    }
+
+}
