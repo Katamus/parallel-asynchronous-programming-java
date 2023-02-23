3c786360cadf1cda4ae7a814708523b7064c4b46
Christian <christiancatamuscay@r-fast.com>
2023-02-22 20:35:24 -0500

    48. Handle/Recover Exceptions using handle function(Sección 15: Exception Handling/Recovery in CompletableFuture)

---
+++ parallel-asynchronous-using-java/src/test/java/com/learnjava/Completablefuture/CompletableFutureHelloWorldExceptionTest.java
@@ -0,0 +1,34 @@
+package com.learnjava.Completablefuture;
+
+import com.learnjava.service.HelloWorldService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CompletableFutureHelloWorldExceptionTest {
+
+    @Mock
+    HelloWorldService helloWorldService = mock(HelloWorldService.class);
+
+    @InjectMocks
+    CompletableFutureHelloWorldException hwcfe;
+
+    @Test
+    void helloworld_3_async_calls_handle() {
+
+        when(helloWorldService.hello()).thenThrow(new RuntimeException(" Exception Runtime"));
+        when(helloWorldService.world()).thenCallRealMethod();
+
+        String result = hwcfe.helloworld_3_async_calls_handle();
+
+        assertEquals(" WORLD! HI COMPLETABLEFUTURE",result);
+
+    }
+}
